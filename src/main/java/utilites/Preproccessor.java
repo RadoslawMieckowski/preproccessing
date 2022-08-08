5d4739b6ec7d2d804cf2cc19f159e4893644c828
Radosław Mieckowski <rmieckowski@wp.pl>
2022-08-08 17:49:18 +0200

    change pom.xml and add readFile method in Preproccessor.java

--- src/main/java/utilites/Preproccessor.java
+++ src/main/java/utilites/Preproccessor.java
@@ -1,5 +1,12 @@
 package utilites;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public final class Preproccessor {
 
     private Preproccessor() {
@@ -8,4 +15,13 @@ public final class Preproccessor {
     public static void preproccessFile() {
 
     }
+
+    public static List<String> readFile(String fileName) throws IOException {
+        List<String> result;
+        try (Stream<String> lines = Files.lines(Paths.get(fileName))) {
+            result = lines.collect(Collectors.toList());
+        }
+        return result;
+    }
+
 }
