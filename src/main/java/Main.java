cf0db8697955c427ee89f032cf202af5d92dc45d
Radosław Mieckowski <rmieckowski@wp.pl>
2022-08-10 20:44:16 +0200

    change Main.java, clean resources directory

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,26 @@
+import utilites.Calculator;
 import utilites.Preproccessor;
 
+import java.io.File;
 import java.nio.file.Path;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        final String sourceDirectory = "C:\\Users\\Admin\\Desktop\\Praca magisterska R\\data\\RNN\\Data\\Stock_price_prediction\\Data\\Stocks";
+        final String targetDirectory = "C:\\Users\\Admin\\Desktop\\Praca magisterska R\\data\\RNN\\Data\\Stock_price_prediction\\Data\\PreproccessedStocks";
         try {
-            List<String[]> lines = Preproccessor.readFile("src/main/resources/a.us.txt");
-            List<String[]> newLines = Preproccessor.preproccessList(lines);
-            Preproccessor.writeAllLinesToFile(newLines, Path.of("src/main/output/a.us.txt"));
+            String[] pathnames;
+            File f = new File(sourceDirectory);
+            pathnames = f.list();
+            for (String fileName : pathnames) {
+                Calculator.setRSI_COUNTER(0);
+                Calculator.setSMA_15_COUNTER(0);
+                Calculator.setSMA_45_COUNTER(0);
+                List<String[]> lines = Preproccessor.readFile(sourceDirectory + "\\" + fileName);
+                List<String[]> newLines = Preproccessor.preproccessList(lines);
+                Preproccessor.writeAllLinesToFile(newLines, Path.of(targetDirectory + "\\" + fileName));
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
